479d7dd0ffc369ca8fb8117353d1de9ed8f49ec9
dedestem <david@davidnet.net>
2025-08-02 13:07:55 +0200

    Svelte-UI Working correctly

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { ThemeProvider, Toaster, ConnectivityCheck, ThemeMenu } from "@davidnet/svelte-ui";
+</script>
+
+<ThemeProvider />
+<Toaster />
+<ConnectivityCheck />
+<ThemeMenu/>
+<slot />
