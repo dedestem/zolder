87095ab90efe245af9840609cbfd78b7762cfc6e
dedestem <david@davidnet.net>
2025-08-02 20:45:43 +0200

    Add functionality to delete an item.

--- src/routes/api/box/[id]/+server.ts
+++ src/routes/api/box/[id]/+server.ts
@@ -27,3 +27,21 @@ export const GET: RequestHandler = async ({ params }) => {
 		headers: { 'Content-Type': 'application/json' }
 	});
 };
+
+export const DELETE: RequestHandler = async ({ params }) => {
+	const id = Number(params.id);
+	if (isNaN(id)) {
+		return new Response('Invalid id', { status: 400 });
+	}
+
+	const db = await openDb();
+
+	const box: Box | undefined = await db.get(`SELECT * FROM boxes WHERE id = ?`, id);
+	if (!box) {
+		return new Response('Box not found', { status: 404 });
+	}
+
+	await db.run(`DELETE FROM boxes WHERE id = ?`, id);
+
+	return new Response(null, { status: 204 });
+};
--- src/routes/box/[id]/+page.svelte
+++ src/routes/box/[id]/+page.svelte
@@ -2,8 +2,10 @@
 	import {
 		Button,
 		FlexWrapper,
+		IconButton,
 		LinkButton,
 		Loader,
+		Modal,
 		Space,
 		TextField,
 		toast
@@ -13,12 +15,14 @@
 	import { onMount } from 'svelte';
 	import Icon from '$lib/components/Icon.svelte';
 	import { formatDate } from '$lib/utils';
+	import { goto } from '$app/navigation';
 
 	let id: string | undefined;
 	let error: string = 'Er ging iets fout.';
 	let box: BoxWithItems | null = null;
 	let Loading = true;
 	let newitemname: string = '';
+	let showDeleteBoxModal = false;
 
 	onMount(async () => {
 		id = page.params.id;
@@ -117,11 +121,106 @@
 				desc: `"${createdItem.name}" is toegevoegd aan doos #${box.id}.`,
 				appearance: 'success',
 				position: 'bottom-left',
-				icon: "box_add",
+				icon: 'contextual_token_add',
 				autoDismiss: 3000
 			});
 		} catch (error) {
-			console.error('Error creating item:', error);
+			toast({
+				title: 'Kon item niet toevoegen',
+				desc: 'Netwerk Fout',
+				icon: 'deployed_code_alert',
+				appearance: 'danger',
+				position: 'bottom-left'
+			});
+		}
+	}
+
+	async function DeleteBox() {
+		if (!id) {
+			return;
+		}
+
+		try {
+			const response = await fetch(`/api/box/${id}`, {
+				method: 'DELETE'
+			});
+
+			if (!response.ok) {
+				const errorText = await response.text();
+				toast({
+					title: 'Kon doos niet verwijderen',
+					desc: 'Error: ' + response.status + ' | ' + response.statusText,
+					icon: 'deployed_code_alert',
+					appearance: 'danger',
+					position: 'bottom-left'
+				});
+				console.error('Error deleting box:', errorText);
+				return;
+			}
+
+			toast({
+				title: `Doos verwijderd.`,
+				desc: `Doos #${id} is verwijderd.`,
+				appearance: 'success',
+				position: 'bottom-left',
+				icon: 'delete_forever',
+				autoDismiss: 7000
+			});
+
+			goto('/');
+		} catch (error) {
+			console.error('Network error:', error);
+			toast({
+				title: 'Kon doos niet verwijderen',
+				desc: 'Netwerk Fout',
+				icon: 'deployed_code_alert',
+				appearance: 'danger',
+				position: 'bottom-left'
+			});
+		}
+	}
+
+	async function DeleteItem(id: number, name: string) {
+		try {
+			const res = await fetch(`/api/item/${id}`, {
+				method: 'DELETE'
+			});
+
+			if (!res.ok) {
+				const errorText = await res.text();
+				toast({
+					title: 'Kon item niet verwijderen',
+					desc: 'Error: ' + res.status + ' | ' + res.statusText,
+					icon: 'deployed_code_alert',
+					appearance: 'danger',
+					position: 'bottom-left'
+				});
+				console.error('Error deleting item:', errorText);
+				return;
+			}
+
+			const result = await res.json();
+			console.log('Item deleted:', result);
+
+			toast({
+				title: `Item verwijderd.`,
+				desc: `Item #${name} is verwijderd.`,
+				appearance: 'info',
+				position: 'bottom-left',
+				icon: 'delete_forever',
+				autoDismiss: 4000
+			});
+			if (box) {
+				box.items = box.items.filter((item) => item.id !== id);
+			}
+		} catch (err) {
+			toast({
+				title: 'Kon item niet verwijderen',
+				desc: 'Netwerk Fout',
+				icon: 'deployed_code_alert',
+				appearance: 'danger',
+				position: 'bottom-left'
+			});
 		}
 	}
 </script>
@@ -148,19 +247,36 @@
 	<Space height="var(--token-space-6)" />
 	{#if box.items.length > 0}
 		<h2>Items in doos:</h2>
-		<ul>
+		<div class="itemlist">
 			{#each box.items as item}
-				<li>
-					<strong>{item.name}</strong> — aangemaakt op {formatDate(item.created_at)}
-					<Space height="var(--token-space-2)"/>
-				</li>
+				<div class="item">
+					<strong>{item.name}</strong> — {formatDate(item.created_at)}
+					<IconButton
+						icon="delete_forever"
+						appearance="danger"
+						alt="delete item "
+						onClick={() => {
+							DeleteItem(item.id, item.name);
+						}}
+					/>
+				</div>
 			{/each}
-		</ul>
+		</div>
 	{:else}
 		<p>Deze doos is leeg.</p>
 	{/if}
+
+	<Space height="var(--token-space-6)" />
+	<Button
+		appearance="danger"
+		onClick={() => {
+			showDeleteBoxModal = true;
+		}}
+		iconbefore="delete_forever">Verwijder Doos</Button
+	>
 {:else}
 	<Icon color="var(--token-color-text-danger)" icon="deployed_code_alert" size="10rem" />
+	<Space height="var(--token-space-6)" />
 	<span>{error}</span>
 	<Space height="var(--token-space-6)" />
 	<FlexWrapper direction="row" gap="10px">
@@ -175,3 +291,32 @@
 		</Button>
 	</FlexWrapper>
 {/if}
+
+{#if showDeleteBoxModal}
+	<Modal
+		title="Doos verwijderen?"
+		titleIcon="delete_forever"
+		desc="Weet je het zeker? De fysieke poster wordt ongeldig."
+		hasCloseBtn
+		on:close={() => (showDeleteBoxModal = false)}
+		options={[
+			{ appearance: 'danger', content: 'Doos verwijderen', onClick: DeleteBox },
+			{ appearance: 'subtle', content: 'Annuleren', onClick: () => (showDeleteBoxModal = false) }
+		]}
+	/>
+{/if}
+
+<style>
+	.item {
+		background-color: var(--token-color-surface-sunken-normal);
+		padding: 1rem 1rem;
+		margin: 0px;
+		border-radius: 20px;
+	}
+
+	.itemlist {
+		display: flex;
+		flex-direction: column;
+		gap: var(--token-space-3);
+	}
+</style>
